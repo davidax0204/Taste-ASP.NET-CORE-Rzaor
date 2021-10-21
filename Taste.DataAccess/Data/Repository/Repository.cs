376fc5ac94b35e88299158c606c9fdc07eb23c87
davidax0204 <71772975+davidax0204@users.noreply.github.com>
2021-10-21 15:53:22 +0300

    Repository done

---
+++ Taste.DataAccess/Data/Repository/Repository.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.DataAccess.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+        internal DbSet<T> dbset;
+
+        // Dependency Injection
+        public Repository(DbContext context)
+        {
+            Context = context;
+            this.dbset = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return dbset.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbset;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            // cinclue properties will be comma sepertaed
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbset;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            // cinclue properties will be comma sepertaed
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = dbset.Find(id);
+            Remove(entityToRemove);
+        }
+
+        public void Remove(T entity)
+        {
+            dbset.Remove(entity);
+        }
+    }
+}
---
+++ Taste.DataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.DataAccess.Data.Repository
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Catergory = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Catergory { get; private set; }
+        public ICategoryRepository Category { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
